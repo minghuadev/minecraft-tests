cb8f80b76513985e4c8787922671762a903d8705
minghua <minghuadev@users.noreply.github.com>
2014-11-30 22:47:01 -0800

    other-javasample/boost-spirit/spirit-examples number_list.cpp partial parsing

--- other-javasamples/boost-spirit/spirit-examples/number_list.cpp
+++ other-javasamples/boost-spirit/spirit-examples/number_list.cpp
@@ -30,18 +30,30 @@ using namespace boost::spirit;
 //
 ///////////////////////////////////////////////////////////////////////////////
 bool
-parse_numbers(char const* str, vector<double>& v)
+parse_numbers(char const* str, vector<double>& v, size_t len)
 {
-    return parse(str,
+    const char * s0 = str;
+    const char * s1 = str+len;
+    cout << "str: " << (s1-s0) << endl;
+
+    parse_info<> result = 
+        parse(s0, s1, 
 
         //  Begin grammar
         (
-            real_p[push_back_a(v)] >> *(',' >> real_p[push_back_a(v)]) >> end_p
+            real_p[push_back_a(v)] >> *(',' >> real_p[push_back_a(v)]) >> eps_p
         )
         ,
         //  End grammar
 
-        space_p).full;
+        space_p);
+    cout << "str: " << (s1-s0) << endl;
+    cout << "result:" << endl;
+    cout << "hit: " << result.hit << " full: " << result.full << " length: " << result.length << endl;
+    cout << "stop: " << (result.stop -s0) << endl;
+    string rest(result.stop, s1);
+    cout << "rest: " << rest << " size: " << rest.size() << endl;
+    return result.full;
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -67,7 +79,7 @@ main()
             break;
 
         vector<double> v;
-        if (parse_numbers(str.c_str(), v))
+        if (parse_numbers(str.c_str(), v, str.size() ))
         {
             cout << "-------------------------\n";
             cout << "Parsing succeeded\n";
