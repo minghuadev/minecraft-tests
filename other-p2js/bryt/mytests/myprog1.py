f0951c501c9e50eba790497205be790182636ebe
minghua <minghuadev@users.noreply.github.com>
2015-05-08 22:46:37 -0700

    other-p2js/bryt add mytests/

---
+++ other-p2js/bryt/mytests/myprog1.py
@@ -0,0 +1,26 @@
+import sys 
+import traceback 
+from browser import document 
+from browser import alert 
+import time 
+def write(data): 
+    document['code'].value += data 
+sys.stdout.write = sys.stderr.write = write 
+
+def echo(event): 
+    N = 10000 
+    a = 0 
+    tm0 = time.time() 
+    for i in range(0,N): 
+        a += 1 
+    tms = time.time() - tm0 
+    ostr = " == %s %s %s == " % (document["zone"].value, tm0, tms) 
+    #print 
+    #print "ostr", ostr 
+    #print 
+    sys.stdout.write(ostr) 
+    alert(ostr) 
+    assert 0, "hhhhhhhhhhhhhh errrrrrrrr %s " % tms 
+
+document['mybutton'].bind('click', echo) 
+
---
+++ other-p2js/bryt/mytests/myprog2.py
@@ -0,0 +1,39 @@
+
+
+console.html:
+
+    <input id="zone"><button id="mybutton">click !</button>
+
+
+$ cat tests/myprog2.py
+    import sys
+    import traceback
+
+    from browser import document
+    from browser import alert
+    import time
+
+    def write(data):
+        document['code'].value += data
+    sys.stdout.write = sys.stderr.write = write
+
+    def echo(event):
+                N = 10000
+                a = 0
+                tm0 = time.time()
+                for i in range(0,N):
+                    a += 1
+                tms = time.time() - tm0
+
+                ostr = " == %s  %s  %s == " % (document["zone"].value, tm0, tms)
+                #print "abcd"
+                #print "ostr", ostr
+                #print
+                sys.stdout.write(ostr)
+                alert(ostr)
+                assert 0, "hhhhhhhhhhhhhh errrrrrrrr %s " % tms
+
+
+    document['mybutton'].bind('click', echo)
+
+
