bffc128a8a5ce48908da6b745faf82b5adab104b
minghuadev <minghuadev@users.noreply.github.com>
2023-03-22 19:23:55 +0000

    other-pyw rpyc tool-dep.py added

---
+++ other-pyw/rpc/tool-dep.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# tool-dep.py
+#
+# ref: stackoverflow questions/41816693
+#      how-to-list-dependencies-for-a-python-library-without-installing
+
+import requests
+
+
+def find_dep(n, v=None):
+    if v is None:
+        url = 'https://pypi.org/pypi/%s/json' % n
+    else:
+        url = 'https://pypi.org/pypi/%s/%s/json' % ( n, v )
+
+    resp = requests.get(url)
+    print("package: ", n, "" if n is None else v )
+    print("resp: ", repr(resp))
+    json = resp.json()
+    #print("json: ", repr(json))
+    required_pkgs = json['info']['requires_dist']
+    print("deps: ", repr(required_pkgs))
+
+
+pkgn="rpyc"
+pkgv="4.1.5"
+find_dep(pkgn, pkgv)
+
+pkgn="plumbum"
+pkgv="1.6.9"
+find_dep(pkgn, pkgv)
+
+
