84baee8807c0f746edfc2710412490657829ded3
minghuadev <minghuadev@users.noreply.github.com>
2025-04-21 19:58:43 +0000

    other-javasamples cloud-samples samples-cloud-8- add android-studio-2024-0420

---
+++ other-javasamples/cloud-samples/samples-cloud-8-android-ble/android-studio-2024-0420/studio64conf/runstudio64.bat
@@ -0,0 +1,12 @@
+
+rem modify idea.config.path in properties file under studio bin
+rem modify idea.system.path in properties file under studio bin
+rem add gradle.user.home in properties file under studio bin
+rem add MAVEN_REPOSITORY in studio settings
+rem set gradle home in studio settings
+rem set avd varaible below
+
+set ANDROID_SDK_HOME=C:\workspace\buildtools\AndroidAvd
+C:\workspace\downloads\android-studio-2024.3.1.14-windows\android-studio\bin\studio64.exe
+
+
